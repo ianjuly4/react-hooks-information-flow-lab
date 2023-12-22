731cd69e4ba3ce90a52a01dee76c7ae401cc8da1
Ian July <ianjuly4@gmail.com>
2023-12-21 17:08:24 -0700

    info flow lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import Filter from"./Filter";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
@@ -13,16 +14,10 @@ function ShoppingList({ items }) {
 
     return item.category === selectedCategory;
   });
-
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+      <div >
+        <Filter onCategoryChange={handleCategoryChange}/>
       </div>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
@@ -34,3 +29,9 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+
+
+ 
+
+
